e75d2ca00ab93a7b622a9fe1f763945030a6fc15
ogembo <60712273+ZEZE1020@users.noreply.github.com>
2024-10-19 04:29:18 +0300

    Task02

---
+++ task_2.sql
@@ -0,0 +1,41 @@
+-- Create the Authors table
+CREATE TABLE Authors (
+  author_id INT AUTO_INCREMENT PRIMARY KEY,
+  author_name VARCHAR(215) NOT NULL
+);
+
+-- Create the Books table
+CREATE TABLE Books (
+  book_id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(130) NOT NULL,
+  author_id INT,
+  price DOUBLE NOT NULL,
+  publication_date DATE,
+  FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+);
+
+-- Create the Customers table
+CREATE TABLE Customers (
+  customer_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_name VARCHAR(215) NOT NULL,
+  email VARCHAR(215) NOT NULL UNIQUE,
+  address TEXT
+);
+
+-- Create the Orders table
+CREATE TABLE Orders (
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT,
+  order_date DATE NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- Create the Order_Details table
+CREATE TABLE Order_details (
+  orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE NOT NULL,
+  FOREIGN KEY (order_id) references Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
