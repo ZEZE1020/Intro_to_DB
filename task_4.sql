a55b62417018e91ec741bff8a64b77f19500362f
ogembo <60712273+ZEZE1020@users.noreply.github.com>
2024-10-19 04:45:51 +0300

    Task04

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,15 @@
+-- Print the full description of the books table
 USE alx_book_store;
 
-DESCRIBE Books; 
+SELECT
+  COLUMN_NAME,
+  COLUMN_TYPE,
+  IS_NULLABLE,
+  COLUMN_KEY,
+  COLUMN_DEFAULT,
+  EXTRA
+FROM
+  INFORMATION_SCHEMA.COLUMNS
+WHERE
+  TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
