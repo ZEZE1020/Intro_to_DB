73d7618f74e658d86c8cfac7b89bbdf38083dd3e
ogembo <60712273+ZEZE1020@users.noreply.github.com>
2024-10-19 05:00:57 +0300

    task06

--- task_6.sql
+++ task_6.sql
@@ -10,17 +10,17 @@ INSERT INTO customers (
   2,
   "Blessing Malik",
   "bmalik@sandtech.com",
-  "124 Happiness Ave."
+  "124 Happiness  Ave."
 ),
 (
   3,
   "Obed Ehoneah",
   "eobed@sandtech.com",
-  "125 Happiness Ave."
+  "125 Happiness  Ave."
 ),
 (
   4,
   "Nehemial Kamolu",
   "nkamolu@sandtech.com",
-  "126 Happiness Ave."
+  "126 Happiness  Ave."
 );
