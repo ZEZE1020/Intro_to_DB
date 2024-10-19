6e1e468829fc02e98476bec12e4a23bed169a3e4
ogembo <60712273+ZEZE1020@users.noreply.github.com>
2024-10-19 04:00:42 +0300

    Se

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,13 +4,13 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 
-CREATE TABLE authors (
+CREATE TABLE Authors (
   author_id INT AUTO_INCREMENT PRIMARY KEY,
   author_name VARCHAR(215) NOT NULL
 );
 
 
-CREATE TABLE books (
+CREATE TABLE Books (
   book_id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(130) NOT NULL,
   author_id INT,
@@ -20,7 +20,7 @@ CREATE TABLE books (
 );
 
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
   customer_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_name VARCHAR(215) NOT NULL,
   email VARCHAR(215) NOT NULL UNIQUE,
@@ -28,7 +28,7 @@ CREATE TABLE customers (
 );
 
 
-CREATE TABLE orders (
+CREATE TABLE Orders (
   order_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_id INT,
   order_date DATE NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE orders (
 );
 
 
-CREATE TABLE order_details (
+CREATE TABLE Order_Details (
   orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
   order_id INT,
   book_id INT,
